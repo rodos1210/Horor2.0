ce80acb9410a5b7bd6ee16b578153f998948d2da
rodos1210 <rodion_nabokih@mail.ru>
2025-04-11 19:37:16 +0300

    Svet and Upravlenue
    Доделал свет, сделал управление + дохуя багов
    -баг с камерой видно сквозь стены
    -баг с объектами на сцене проваливаешься под карту при столкновении
    -баг с дверьми какото хуя не получается пройти через нее

---
+++ hroro/Assets/SCRIPTS/Player movement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float walkspeed = 2f;
+    [SerializeField] private float runspeed = 3f;
+    [SerializeField] private float gravity = -9.8f;
+
+    [Header("Camera")]
+    [SerializeField] private Camera virtualcamera;
+    [SerializeField] private Transform camerapivot;
+    [SerializeField] private float mouseSensivity = 2f;
+
+    private CharacterController controller;
+    private float vertikalSpeed;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        HendleLook();
+        HandleMove();
+    }
+
+    private void HendleLook()
+    {
+        float mousex = Input.GetAxis("Mouse X") * mouseSensivity;
+        float mousey = Input.GetAxis("Mouse Y") * mouseSensivity;
+        transform.Rotate(Vector2.up * mousex);
+
+        float vertivalRotation = -mousey; // координаты мыши 2д
+        float currentCAmeraAngel = camerapivot.localEulerAngles.x; // координаты камеры 3д + угол наклона
+        float newAngel = currentCAmeraAngel + vertivalRotation;
+        if (newAngel > 180)
+        {
+            newAngel -= 360;
+            newAngel = Mathf.Clamp(newAngel, -90, 90); // не дает выйти за пределы 90 и -90
+        }
+
+        camerapivot.localEulerAngles = new Vector3(newAngel, 0, 0);
+    }
+    private void HandleMove()
+    {
+        float speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runspeed;
+        }
+        else
+        {
+            speed = walkspeed;
+        }
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Vector3 move = transform.TransformDirection(input) * speed;
+
+        if (controller.isGrounded && vertikalSpeed < 0)
+        {
+            vertikalSpeed = -2f;
+        }
+        vertikalSpeed += gravity * Time.deltaTime;
+        move.y = vertikalSpeed;
+        controller.Move(move * Time.deltaTime);
+    }
+}
