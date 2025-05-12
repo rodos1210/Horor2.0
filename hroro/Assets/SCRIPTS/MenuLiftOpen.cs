4a6a730053bb36f986c278078e5103d313313809
Mentol0408 <hubg14959@gmail.com>
2025-05-12 17:08:35 +0300

    fdfd
    опен лифт двери

---
+++ hroro/Assets/SCRIPTS/MenuLiftOpen.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class MenuLiftOpen : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100))
+        {            
+            if (hit.collider.gameObject.tag == "Door")
+            {
+                animator.SetTrigger("menuLiftOpen");
+            }
+            
+        }
+    }
+}
