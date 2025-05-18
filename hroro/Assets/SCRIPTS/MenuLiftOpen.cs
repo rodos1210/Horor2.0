3ec932689432d0d5c23526ac274a5a63c275864d
nonentityy <egor228.prihodkoo@gmail.com>
2025-05-18 23:21:10 +0300

    ffdfdffd
    почти готова менюшка

--- hroro/Assets/SCRIPTS/MenuLiftOpen.cs
+++ hroro/Assets/SCRIPTS/MenuLiftOpen.cs
@@ -18,12 +18,19 @@ public class MenuLiftOpen : MonoBehaviour
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit, 100))
-        {            
-            if (hit.collider.gameObject.tag == "Door")
+        {
+            if (hit.collider.gameObject.CompareTag("Door"))
             {
+                animator.ResetTrigger("menuLiftClosee");
                 animator.SetTrigger("menuLiftOpen");
             }
-            
+            else
+            {
+
+                animator.ResetTrigger("menuLiftOpen");
+                animator.SetTrigger("menuLiftClosee");
+            }
         }
+        
     }
 }
